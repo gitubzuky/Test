3e935801d25279d197b1bb1f84b3ce49e583d347
zuky <master_go@163.com>
2017-11-03 16:21:48 +0800

    ndk:分别测试java调用c++代码和c++调用java代码

--- mylib/src/main/java/com/lib/mylib/util/JniUtil.java
+++ mylib/src/main/java/com/lib/mylib/util/JniUtil.java
@@ -6,13 +6,14 @@ package com.lib.mylib.util;
  */
 
 public class JniUtil {
+//    private static JniUtil obj = new JniUtil();
+    private CppCallListener listener;
 
-    private static JniUtil obj = new JniUtil();
-    private JniUtil(){}
+//    private JniUtil(){}
 
-    public static JniUtil instance(){
-        return obj;
-    }
+//    public static JniUtil instance(){
+//        return obj;
+//    }
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("ndk-lib");
@@ -22,7 +23,21 @@ public class JniUtil {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public static native String stringFromJNI1(int position);
+    public native String stringFromJNI1(int position);
 
-    public static native String stringFromJNI2();
+    public native void stringFromJNI2(int position);
+
+    void callByCpp(int num){
+        if (listener != null){
+            listener.onCppCall(num);
+        }
+    }
+
+    public void setCppCallListener(CppCallListener listener){
+        this.listener = listener;
+    }
+
+    public interface CppCallListener{
+        void onCppCall(int num);
+    }
 }
